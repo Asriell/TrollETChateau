c8ffde23467fc29effe520126a2c02f2cebf9f93
Kevin BURDIN <k.burdin@hotmail.fr>
2021-06-22 02:12:17 +0200

    modifications TP

--- JeuDuTroll/JeuDuTroll.py
+++ JeuDuTroll/JeuDuTroll.py
@@ -1,5 +1,6 @@
 # module qui met en place l'ensemble de l'environnement du jeu du troll, ainsi que le lancement des parties
 import Strategies as strat
+import random
 
 class Plateau : #Objet qui va contenir une situation de jeu (nombre de pierres pour chaque joueur, nombre de cases, position du troll.
     nbCases = 7
@@ -41,7 +42,8 @@ def Partie(mode,_nbCases,_posTrollDepart,_pierresJ1Depart,_pierresJ2Depart,Coups
             CoupsJ1.append(CoupJ1)
         else :  # si ce n'est pas un joueur, il faut appeler une fonction qui renvoie le nombre de pierres lancees, elle sont dans le module Strategies.
             #CoupJ1 = strat.StrategieAleatoire(plateau.nbPierresJoueur1)
-            CoupJ1 = strat.StrategiePrudente(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+            #CoupJ1 = strat.StrategiePrudente(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+            CoupJ1 = strat.StrategieTest()
             #CoupJ1 = strat.StrategieAgressive(_pierresJ1Depart,plateau.nbCases,plateau.nbPierresJoueur1)
             #CoupJ1 = strat.Strategie1(15,7,plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.posTroll,1)
             CoupsJ1.append(CoupJ1)
@@ -61,13 +63,88 @@ def Partie(mode,_nbCases,_posTrollDepart,_pierresJ1Depart,_pierresJ2Depart,Coups
         else : # si ce n'est pas un joueur, il faut appeler une fonction qui renvoie le nombre de pierres lancees, elle sont dans le module Strategies.
             #CoupJ2 = strat.StrategieAgressive(_pierresJ2Depart,plateau.nbCases,plateau.nbPierresJoueur2)
             CoupJ2 = strat.StrategieAleatoire(plateau.nbPierresJoueur2)
-            #CoupJ2 = strat.Strategie1(15,7,plateau.nbPierresJoueur2,plateau.nbPierresJoueur1,plateau.posTroll,2)
             #CoupJ2 = strat.StrategiePrudenteJ2(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+            #CoupJ2 = strat.Strategie1(15,7,plateau.nbPierresJoueur2,plateau.nbPierresJoueur1,plateau.posTroll,2)
+            #CoupJ2 = strat.StrategiePrudenteNonLineaireJ2(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+            #CoupJ2 = strat.StrategieTest()
             CoupsJ2.append(CoupJ2)
         plateau.nbPierresJoueur1 -= CoupJ1
         plateau.nbPierresJoueur2 -= CoupJ2
         if CoupJ1 > CoupJ2:
             plateau.posTroll += 1 
+        elif CoupJ1 < CoupJ2:
+            plateau.posTroll -= 1 
+        if (plateau.nbPierresJoueur1 <= 0 or plateau.nbPierresJoueur2 <= 0 or plateau.posTroll == plateau.nbCases or plateau.posTroll == 1) : # Si un joueur n'a plus de pierre, ou si le troll est arrive a destination, la partie s'arrete. 
+            fin = True
+    return ElectionJoueurGagnant(plateau) # 1 = joueur 1 a gagne, 2 = joueur 2 a gagne, 0 = match nul
+
+def PartieModifiee(mode,_nbCases,_posTrollDepart,_pierresJ1Depart,_pierresJ2Depart,probaJ2,CoupsJ1 = [],CoupsJ2 = []) : # Lancement d'une partie en fonction du mode de jeu (combien de joueurs), des differents parametres de jeu et de deux tableaux, en reference, pour l'historique des coups joues
+    plateau = Plateau(_nbCases,_posTrollDepart,_pierresJ1Depart,_pierresJ2Depart)
+    print(plateau.nbCases," ",plateau.posTroll," ",plateau.nbPierresJoueur1," ",plateau.nbPierresJoueur2) # affichage de la situation initiale
+    fin = False
+    while not fin :
+        print(plateau.nbCases, "cases, Troll a la position : ", plateau.posTroll, " J1 : ",plateau.nbPierresJoueur1," pierres, J2 : ",plateau.nbPierresJoueur2," pierres") #affichage de la situation courante
+
+        if mode < 2 : # La variable "mode" de la Partie : 0 = 2 joueurs, 1 = 1 joueur face a une IA, 2 = 2 IAs.
+            bon = False
+            while not bon :
+                print("Joueur 1, rentrez une valeur !")
+                StrCoupJ1 = input()
+                try : #saisie du coup par le joueur 1
+                     CoupJ1 = int(StrCoupJ1)
+                     if CoupJ1 <= plateau.nbPierresJoueur1 and CoupJ1 > 0 :
+                         bon = True
+                except ValueError:
+                    print("entrez une valeur entiere svp")
+            CoupsJ1.append(CoupJ1)
+        else :  # si ce n'est pas un joueur, il faut appeler une fonction qui renvoie le nombre de pierres lancees, elle sont dans le module Strategies.
+            #CoupJ1 = strat.StrategieAleatoire(plateau.nbPierresJoueur1)
+            CoupJ1 = strat.StrategiePrudenteModifiee(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+            #CoupJ1 = strat.StrategieTest()
+            #CoupJ1 = strat.StrategieAgressive(_pierresJ1Depart,plateau.nbCases,plateau.nbPierresJoueur1)
+            #CoupJ1 = strat.Strategie1(15,7,plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.posTroll,1)
+            CoupsJ1.append(CoupJ1)
+
+        if mode == 0 :
+            bon = False
+            while not bon :
+                print("Joueur 2, rentrez une valeur !")
+                StrCoupJ2 = input()
+                try : #saisie du coup par le joueur 2
+                     CoupJ2 = int(StrCoupJ2)
+                     if CoupJ2 <= plateau.nbPierresJoueur2 and CoupJ2 > 0 and CoupJ2 % 2 == 1 :
+                         bon = True
+                except ValueError:
+                    print("entrez une valeur entiere svp")
+            CoupsJ2.append(CoupJ2)
+        else : # si ce n'est pas un joueur, il faut appeler une fonction qui renvoie le nombre de pierres lancees, elle sont dans le module Strategies.
+            bon = False
+            stop = 0
+            while not bon : 
+                #CoupJ2 = strat.StrategieAgressive(_pierresJ2Depart,plateau.nbCases,plateau.nbPierresJoueur2)
+                CoupJ2 = strat.StrategieAleatoire(plateau.nbPierresJoueur2)
+                #CoupJ2 = strat.StrategiePrudenteJ2(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+                #CoupJ2 = strat.Strategie1(15,7,plateau.nbPierresJoueur2,plateau.nbPierresJoueur1,plateau.posTroll,2)
+                #CoupJ2 = strat.StrategiePrudenteNonLineaireJ2(plateau.nbPierresJoueur1,plateau.nbPierresJoueur2,plateau.nbCases,plateau.posTroll)
+                #CoupJ2 = strat.StrategieTest()
+                stop +=1
+                assert stop <=500, "Valeur impaire non trouvee"
+                if CoupJ2 % 2 == 1:
+                    bon = True
+            CoupsReussisJ2 = 0
+            for i in range(CoupJ2) :
+                rand = random.uniform(0,1)
+                if rand <= probaJ2 :
+                    CoupsReussisJ2 += 1
+            CoupJ2 = CoupsReussisJ2
+            CoupsJ2.append(CoupJ2)
+        plateau.nbPierresJoueur1 -= CoupJ1
+        plateau.nbPierresJoueur2 -= CoupJ2
+        if CoupJ1 > CoupJ2:
+            if CoupJ1 > CoupJ2 + 2 :
+                plateau.posTroll += 2
+            else : 
+                plateau.posTroll += 1 
         elif CoupJ1 < CoupJ2:
             plateau.posTroll -= 1 
         if (plateau.nbPierresJoueur1 <= 0 or plateau.nbPierresJoueur2 <= 0 or plateau.posTroll == plateau.nbCases or plateau.posTroll == 1) : # Si un joueur n'a plus de pierre, ou si le troll est arrive a destination, la partie s'arrete. 
--- JeuDuTroll/main.py
+++ JeuDuTroll/main.py
@@ -1,13 +1,88 @@
 import JeuDuTroll as troll
+import numpy as np
 import time
 
 s1 = 0 # nombre de fois ou la strategie du joueur 1 a gagne 
 s2 = 0 # nombre de fois ou la strategie du joueur 2 a gagne
 matchsNuls = 0 # nombre de matchs nuls
 
+# Parametres du jeu 
+ModeDeJeu = 2
+NbCases = 5
+PositionTroll = 3
+NbPierresJ1 = 20
+NbPierresJ2 = 20 # Pour changer la strategie de chaque joueur, il faut changer la fonction en commentaire dans JeuDuTroll pour chaque joueur.
+ProbaSuccesJ2 = 0.5
+
+
+def Exercice3(nbPierresJ1 = 20,nbPierresJ2=20,posTroll=3,nbCases=5) :
+    print("_____________Exercice 3_________________")
+    matrice = []
+    for i in range(nbPierresJ1) :
+        col = []
+        for j in range(nbPierresJ2) :
+            col.append(float("inf"))
+        matrice.append(col)
+
+    troll.strat.simplex.MatriceGains(nbPierresJ1,nbPierresJ2,posTroll,nbCases,matrice) # calcul matrice de gains
+    probas = troll.strat.simplex.solve(matrice) # calcul des differentes probabilites de jouer une situation donnee
+    #print(probas)
+    probasSansGain = np.delete(probas.x,len(probas.x)-1) # retrait du gain (il s'agit du gain dans le pire des cas, nous, nous cherchons le gain lorsque le joueur adverse joue aleatoirement
+    gains = 0
+    nbMaxPierresLanceesJ2 = nbPierresJ2//3
+    nbPierresRestantesJ2 = nbPierresJ2 - nbMaxPierresLanceesJ2
+    probasLancerPierres = 1/nbMaxPierresLanceesJ2
+    print("pierres restantes : ",nbPierresRestantesJ2)
+    print("pierres lancees : ",nbMaxPierresLanceesJ2)
+    print("pierres lancees : ",probasLancerPierres)
+    for i in range(len(matrice)):
+        proba = probasSansGain[len(probasSansGain)-1]
+        sum = 0
+        for j in range(len(matrice[0])) : 
+# avec Pi la probabilite de lancer n1-i pierres pour le joueur1,Pj la probabilite de lancer n2-j pierres pour le joueur2, et Gij le gain quand il reste i pierres au J1 et j pierres au J2, G = somme des i (Xi*(somme des j (Xj*Gij)))
+            if j >= nbPierresRestantesJ2: # Sinon, la probabilite de lancer est de 0 pour le joueur 2.
+                sum += matrice[i][j] * probasLancerPierres
+        gains += sum * probasSansGain[i]
+    print("gains contre la strategie aleatoire n/3 : ",gains)
+
+
+    
+
+
+def Exercice1() :
+    print("_____________Exercice 1_________________")
+    res = troll.strat.simplex.ProbasPourUneSituationDeJeu(23,20,2,5)
+    probasSansGain = np.delete(res.x,len(res.x)-1)
+    print("La distribution de probabilites est la suivante : ")
+    print(probasSansGain)
+    print("le gain est le suivant : ")
+    print(res.x[len(res.x)-1])
+    print("______________________________")
+    nbPierresJ2 = 1
+    gains = 1
+    while True :
+        res = troll.strat.simplex.ProbasPourUneSituationDeJeu(20,nbPierresJ2,2,5)
+        gains = res.x[len(res.x)-1]
+        if gains < 0 :
+            break
+        nbPierresJ2 += 1
+    print(" dans la configuration (20,x,-1), le joueur 1 est désavantagé a partir de x = ",nbPierresJ2)
+    
 
 
 
+
+def Exercice4() :
+    nbPierresJ2 = 1
+    gains = 1
+    while True :
+        res = troll.strat.simplex.ProbasPourUneSituationDeJeuModifiee(20,nbPierresJ2,3,5)
+        gains = res.x[len(res.x)-1]
+        print("x : ",nbPierresJ2," gains : ",gains)
+        if gains < 0 :
+            break
+        nbPierresJ2 += 1
+    print(" dans la configuration (20,x,-1), le joueur 1 est désavantagé a partir de x = ",nbPierresJ2) # gains négatifs pour x = 56
 
 def main() : # programme principal, en synchrone, pertinent pour l'affichage de toutes les etapes. Suivant le nombre de parties jouees, la fonction peut avoir un temps d'execution tres long, d'où une version asynchrone.
     global s1
@@ -16,11 +91,11 @@ def main() : # programme principal, en synchrone, pertinent pour l'affichage de 
     CoupsJ1 = []
     CoupsJ2 = []
     start = time.perf_counter() # timestamp de debut de programme pour mesurer le temps d'execution
-    for k in range (100) : # iteration sur le nombre de parties, augmenter ce chiffre augmentera fortement le temps d'execution
+    for k in range (1000) : # iteration sur le nombre de parties, augmenter ce chiffre augmentera fortement le temps d'execution
         print ("iteration n° ", k+1)
         CoupsJ1 = []
         CoupsJ2 = []
-        victoire = troll.Partie(2,7,4,10,10,CoupsJ1,CoupsJ2) # 0 = nul, 1 = victoire J1, 2 = victoire J2 | mode,nombre de cases,position du troll, nombre de pierres par joueur, historique 
+        victoire = troll.PartieModifiee(ModeDeJeu,NbCases,PositionTroll,NbPierresJ1,NbPierresJ2,ProbaSuccesJ2,CoupsJ1,CoupsJ2) # 0 = nul, 1 = victoire J1, 2 = victoire J2 | mode,nombre de cases,position du troll, nombre de pierres par joueur, historique 
         print("Liste des coups du joueur 1 : ",CoupsJ1)
         print("Liste des coups du joueur 2 : ",CoupsJ2)
         if victoire == 0 :
@@ -43,4 +118,5 @@ def main() : # programme principal, en synchrone, pertinent pour l'affichage de 
 
 
 
-main()
+#main()
+Exercice4()
